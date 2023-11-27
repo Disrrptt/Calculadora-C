657aa0a863aa511bf5e2581891d09cd4199f5b1a
Disrrptt <gustavovilleth@gmail.com>
2023-11-27 13:18:07 -0300

    finalizado calculadora C

---
+++ pilha.h
@@ -0,0 +1,28 @@
+#ifndef PILHA_H
+#define PILHA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_SIZE 100
+
+typedef struct {
+    char items[MAX_SIZE];
+    int top;
+} Stack;
+
+void initialize(Stack *stack);
+int isEmpty(Stack *stack);
+int isFull(Stack *stack);
+void push(Stack *stack, char item);
+char pop(Stack *stack);
+char peek(Stack *stack);
+int isOperator(char c);
+int getPrecedence(char c);
+void prefixToPostfix(char *prefix, char *postfix);
+double evaluatePostfix(char *postfix);
+
+#endif // PILHA_H
